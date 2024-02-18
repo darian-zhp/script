650af6975a79f39eed43c72971d6d40580b6d5bc
Darian <huip_z@163.com>
2024-02-18 17:15:55 +0800

    [+] New java install script

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # script
-linux script  library
+
+linux script library
+
+## 注意事项
+
+- redis 需要自己配置
+    - 无密码
+    - 无后台启动
+    - 没有开启远程.
+    - 无 redis-cli
+
+- nginx
+    - 之开启了开机启动, 其他无配置.
+
+- openjdk8
+    - 需要自行配置环境变量,将以下内容写入 `/etc/profile`, 然后刷新文件 `source /etc/profile`
+  ```bash
+      export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
+      export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+      export PATH=$PATH:$JAVA_HOME/bin
+  ```
+  
--- cos/openjdk8-install.sh
+++ cos/openjdk8-install.sh
@@ -3,8 +3,9 @@
 yum clean
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
-{
-  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
-  export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-  export PATH=$PATH:$JAVA_HOME/bin
-} >>/etc/profile
+# 环境变量无法写入..., 待优化
+#{
+#  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
+#  export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+#  export PATH=$PATH:$JAVA_HOME/bin
+#} >>/etc/profile
